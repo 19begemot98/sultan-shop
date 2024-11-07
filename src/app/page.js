f7e43a16dedf48123d7d3fdb388febdfa6eaf3c5
Karo-1-4 <karolinaburakova14@gmail.com>
2024-11-07 04:53:48 +0300

    chore: improve code

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Header from './components/header';
+import Banner from '../components/banner';
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <Header />
+      <Banner />
     </div>
   );
 }
